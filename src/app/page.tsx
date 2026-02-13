48853788bb24fd765fcc9a16ad1220f72a1151b2
MinUL <87965992+adunto@users.noreply.github.com>
2026-02-13 19:33:24 +0900

    Main Page (#4)
    * [FEAT] Root Header
    
    * [FEAT] Header Tab (BOARD | MESSENGER)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import RootHeader from "@/components/common/Header";
+import MainContainer from "@/components/common/MainContainer";
+
 export default function HomePage() {
   return (
-    <div>
-      메인화면
-    </div>
+    <>
+      {/* 헤더 */}
+      <RootHeader />
+
+      {/* 메인 컨테이너 */}
+      <MainContainer />
+    </>
   );
 }
---
+++ src/components/feature/board/Board.tsx
@@ -0,0 +1,10 @@
+const Board = () => {
+
+  return (
+    <div className="h-full flex justify-center items-center">
+      게시판
+    </div>
+  )
+}
+
+export default Board;
