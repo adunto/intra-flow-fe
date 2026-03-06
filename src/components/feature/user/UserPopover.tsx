8cdb640bfb7a1074a3f0dab3a20ca91a54f8cd67
Kwon Soonmin <sogno6037@gmail.com>
2026-03-06 09:37:06 +0900

    [FIX] 사용자 Navigation Button

--- src/components/feature/user/UserPopover.tsx
+++ src/components/feature/user/UserPopover.tsx
@@ -4,8 +4,13 @@ import { useQuery } from "@tanstack/react-query";
 import { Loader2, LogOutIcon, UserIcon } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useTransition } from "react";
+import { useState, useTransition } from "react";
 import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { logout } from "@/dal/auth";
 import { getUserInfo } from "@/dal/user";
 import { useAuthStore } from "@/stores/useAuthStore";
@@ -13,15 +18,18 @@ import type { User } from "@/types/user";
 
 const UserPopover = () => {
   const { accessToken, setAccessToken } = useAuthStore();
-  const [startTransition] = useTransition();
+  const [_, startTransition] = useTransition();
   const router = useRouter();
 
+  // 팝업 열림 상태
+  const [open, setOpen] = useState(false);
+
   const { data: user, isPending: isUserPending } = useQuery<User>({
-    queryKey: ["currentUser", accessToken],
+    queryKey: ["currentUser"],
     queryFn: getUserInfo,
+    retry: 1,
     staleTime: 1000 * 60 * 60,
     gcTime: 1000 * 60 * 120,
-    retry: false,
   });
 
   if (isUserPending) {
@@ -33,7 +41,7 @@ const UserPopover = () => {
     );
   }
 
-  if (!accessToken) {
+  if (!user || !accessToken) {
     return (
       <Button variant="default" onClick={() => router.push("/login")}>
         로그인 / 회원가입
@@ -49,6 +57,8 @@ const UserPopover = () => {
       setAccessToken(null);
 
       startTransition(() => {
+        // 팝업에서 로그아웃 한 경우 팝업 닫기
+        setOpen(false);
         router.refresh();
       });
     } else {
@@ -58,28 +68,51 @@ const UserPopover = () => {
 
   return (
     <div className="flex gap-2">
-      <Button variant="ghost" className="flex items-center gap-2 pl-2 pr-4">
-        {/* 프로필 이미지 영역 */}
-        <div className="relative h-8 w-8 overflow-hidden rounded-full border bg-muted">
-          {user?.profileImage ? (
-            <Image
-              src={user.profileImage}
-              alt={user.username}
-              fill
-              className="object-cover"
-              sizes="32px"
-              priority // LCP 최적화
-            />
-          ) : (
-            <div className="flex h-full w-full items-center justify-center bg-gray-100">
-              <UserIcon className="h-5 w-5 text-gray-500" />
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button variant="ghost" className="flex items-center gap-2 pl-2 pr-4">
+            {/* 프로필 이미지 영역 */}
+            <div className="relative h-8 w-8 overflow-hidden rounded-full border bg-muted">
+              {user?.profileImage ? (
+                <Image
+                  src={user.profileImage}
+                  alt={user.username}
+                  fill
+                  className="object-cover"
+                  sizes="32px"
+                  priority // LCP 최적화
+                />
+              ) : (
+                <div className="flex h-full w-full items-center justify-center bg-gray-100">
+                  <UserIcon className="h-5 w-5 text-gray-500" />
+                </div>
+              )}
             </div>
-          )}
-        </div>
 
-        {/* 사용자 이름 */}
-        <span className="text-sm font-medium">{user?.username}</span>
-      </Button>
+            {/* 사용자 이름 */}
+            <span className="text-sm font-medium">{user?.username}</span>
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-30 flex flex-col gap-2 p-2">
+          <Button
+            variant="ghost"
+            className="hover:bg-gray-100"
+            onClick={() => {
+              router.push("/user/info");
+              setOpen(false);
+            }}
+          >
+            내 정보
+          </Button>
+          <Button
+            variant="ghost"
+            className="hover:bg-gray-100"
+            onClick={() => handleLogout()}
+          >
+            로그아웃
+          </Button>
+        </PopoverContent>
+      </Popover>
 
       {/* 로그아웃 버튼 */}
       <Button onClick={() => handleLogout()}>
